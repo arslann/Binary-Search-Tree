b6cdc7c56f4ab6fcff8b25ef4cb22a8ba6eb0f9e
arslann <arslansahin.1907@gmail.com>
2022-01-18 23:52:57 +0300

    completed

--- tree-practice.js
+++ tree-practice.js
@@ -1,75 +1,84 @@
 // Practice problems on binary trees
 
-function findMinBST (rootNode) {
+function findMinBST(rootNode) {
+
+  if (!rootNode.left) return rootNode.val;
+
+
+  let min = findMinBST(rootNode.left)
+
+
+  return min;
+}
+
+function findMaxBST(rootNode) {
+
+  if (!rootNode.right) return rootNode.val;
+
+
+  let max = findMaxBST(rootNode.right)
+
+
+  return max;
+
+}
+
+function findMinBT(rootNode, min = rootNode.val) {
+  // cant fixed bug
+}
+
+function findMaxBT(rootNode) {
 
   // Fill this in
 
 }
 
-function findMaxBST (rootNode) {
+function getHeight(rootNode) {
 
   // Fill this in
 
 }
 
-function findMinBT (rootNode) {
+function countNodes(rootNode) {
 
   // Fill this in
 
 }
 
-function findMaxBT (rootNode) {
+function balancedTree(rootNode) {
 
   // Fill this in
 
 }
 
-function getHeight (rootNode) {
+function getParentNode(rootNode, target) {
 
   // Fill this in
 
 }
 
-function countNodes (rootNode) {
-
-  // Fill this in
-
-}
-
-function balancedTree (rootNode) {
-
-  // Fill this in
-
-}
-
-function getParentNode (rootNode, target) {
-
-  // Fill this in
-
-}
-
-function inOrderPredecessor (rootNode, target) {
+function inOrderPredecessor(rootNode, target) {
 
   // Fill this in
 
 }
 
 
-function deleteNodeBST (rootNode, target) {
+function deleteNodeBST(rootNode, target) {
 
   // Fill this in
 
 }
 
 module.exports = [
-    findMinBST,
-    findMaxBST,
-    findMinBT,
-    findMaxBT,
-    getHeight,
-    countNodes,
-    balancedTree,
-    getParentNode,
-    inOrderPredecessor,
-    deleteNodeBST
+  findMinBST,
+  findMaxBST,
+  findMinBT,
+  findMaxBT,
+  getHeight,
+  countNodes,
+  balancedTree,
+  getParentNode,
+  inOrderPredecessor,
+  deleteNodeBST
 ]
