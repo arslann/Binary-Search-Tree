0e6507a60b8a33b361871f2d98204c887bc5cc65
arslann <arslansahin.1907@gmail.com>
2022-01-18 20:34:51 +0300

    complete binary search tree

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -13,50 +13,113 @@ class BinarySearchTree {
     this.root = null;
   }
 
-  insert(val, currentNode=this.root) {
+  insert(val, currentNode = this.root) {
 
-    // Fill this in
+    if (!this.root) {
+      this.root = new TreeNode(val);
+      return
+    };
+
+
+    if (val < currentNode.val) {
+      if (!currentNode.left) currentNode.left = new TreeNode(val);
+      else this.insert(val, currentNode.left);
+    } else if (val > currentNode.val) {
+      if (!currentNode.right) currentNode.right = new TreeNode(val);
+      else this.insert(val, currentNode.right);
+    }
 
   }
 
-  search(val) {
+  search(target) {
 
-    // Fill this in
+    let currentNode = this.root;
+
+    while (currentNode !== null) {
+      if (target === currentNode.val) return true;
+
+      if (currentNode && target < currentNode.val) currentNode = currentNode.left;
+
+      if (currentNode && target > currentNode.val) currentNode = currentNode.right;
+    }
+
+    return false;
 
   }
 
 
   // Breadth First Traversal - Iterative
-  breadthFirstTraversal() {
+  breadthFirstTraversal(root = this.root) {
 
-    // Fill this in
+    let queue = [root];
+
+    while (queue.length > 0) {
+      let node = queue.shift();
+
+      if (!node) return;
+
+      console.log(node.val);
+
+      queue.push(node.left);
+      queue.push(node.right);
+    }
 
   }
 
   // Depth First Traversal - Iterative
-  depthFirstTraversal() {
+  depthFirstTraversal(root = this.root) {
 
-    // Fill this in
+    const stack = [];
+    stack.push(root);
+
+    // While the stack is not empty
+    while (stack.length > 0) {
+
+      // Pop a node and print it
+      let node = stack.pop();
+      if (!node) return;
+      console.log(node.val);
+
+      // Put all of the node's children on the top of the stack
+      stack.push(node.left);
+      stack.push(node.right);
+    }
 
   }
 
-  preOrderTraversal(currentNode=this.root) {
+  preOrderTraversal(node = this.root) {
 
-    // Fill this in
+    if (!node) return;
 
-  }
+    console.log(node.val);
 
-
-  inOrderTraversal(currentNode=this.root) {
-
-    // Fill this in
+    this.preOrderTraversal(node.left);
+    this.preOrderTraversal(node.right);
 
   }
 
 
-  postOrderTraversal(currentNode=this.root) {
+  inOrderTraversal(node = this.root) {
 
-    // Fill this in
+    if (!node) return;
+
+    this.inOrderTraversal(node.left);
+
+    console.log(node.val);
+
+    this.inOrderTraversal(node.right);
+
+  }
+
+
+  postOrderTraversal(node = this.root) {
+
+    if (!node) return;
+
+    this.postOrderTraversal(node.left);
+    this.postOrderTraversal(node.right);
+
+    console.log(node.val);
 
   }
 
